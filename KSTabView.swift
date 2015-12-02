08974be0846ff45cc3ed6ddf2f5e2c29a24ef0df
Kaunteya Suryawanshi <k.suryawanshi@gmail.com>
2015-12-02 10:03:57 +0530

    Updated the NSBEzierPath to CGPath conversion

--- KSTabView.swift
+++ KSTabView.swift
@@ -11,39 +11,39 @@ import Cocoa
 
 @IBDesignable
 public class KSTabView: NSControl {
-    
+
     enum SelectionType: Int {
         case None =  0, One, Any
     }
-    
+
     @IBInspectable var backgroundColor: NSColor! = NSColor(calibratedRed: 5 / 255, green: 105 / 255, blue: 92 / 255, alpha: 1)
     @IBInspectable var hoverColor: NSColor! = NSColor(calibratedRed: 12 / 255, green: 81 / 255, blue: 68 / 255, alpha: 1)
-    
+
     @IBInspectable var labelColor: NSColor! = NSColor(calibratedRed: 137/255, green: 185/255, blue: 175/255, alpha: 1.0)
     @IBInspectable var selectionColor: NSColor! = NSColor.whiteColor()
-    
+
     @IBInspectable var fontSize: CGFloat = 16
     @IBInspectable var buttonPadding: CGFloat = 10
-    
+
     private var leftButtonList = [KSButton]()
     private var rightButtonList = [KSButton]()
-    
+
     public var leftImagePosition = NSCellImagePosition.ImageLeft
     public var rightImagePosition = NSCellImagePosition.ImageLeft
-    
+
     var selectionType: SelectionType = .One {
         didSet {
             self.selectedButtons = []
         }
     }
-    
+
     public var selectedButtons: [String] {
         get {
             return (leftButtonList + rightButtonList).filter { $0.selected }.map { $0.identifier }.filter{ $0 != nil}.map {$0!}
         }
-        
+
         set(newIdentifierList) {
-            
+
             switch selectionType {
             case .One:
                 if newIdentifierList.count > 1 {
@@ -52,7 +52,7 @@ public class KSTabView: NSControl {
                 }
             default:()
             }
-            
+
             for button in (leftButtonList + rightButtonList) {
                 if let validIdentifier = button.identifier where newIdentifierList.contains(validIdentifier) {
                     button.selected = true
@@ -62,16 +62,16 @@ public class KSTabView: NSControl {
             }
         }
     }
-    
+
     required public init?(coder: NSCoder) {
         super.init(coder: coder)
     }
-    
+
     override public func drawRect(dirtyRect: NSRect) {
         backgroundColor.setFill()
         NSRectFillUsingOperation(dirtyRect, NSCompositingOperation.CompositeSourceOver)
     }
-    
+
     public func removeLeftButtons() -> KSTabView {
         for aButton in leftButtonList {
             aButton.removeFromSuperview()
@@ -79,7 +79,7 @@ public class KSTabView: NSControl {
         leftButtonList.removeAll(keepCapacity: false)
         return self
     }
-    
+
     public func removeRightButtons() -> KSTabView {
         for aButton in rightButtonList {
             aButton.removeFromSuperview()
@@ -87,12 +87,12 @@ public class KSTabView: NSControl {
         rightButtonList.removeAll(keepCapacity: false)
         return self
     }
-    
+
     public func pushButtonLeft(identifier: String, title: String) -> KSTabView {
         _pushButton(identifier, title: title, image: nil, alternateImage: nil, align: .Left)
         return self
     }
-    
+
     public func pushButtonLeft(identifier: String, image: NSImage, alternateImage: NSImage?) -> KSTabView {
         _pushButton(identifier, title: nil, image: image, alternateImage: alternateImage, align: .Left)
         return self
@@ -106,7 +106,7 @@ public class KSTabView: NSControl {
         _pushButton(identifier, title: title, image: nil, alternateImage: nil, align: .Right)
         return self
     }
-    
+
     public func pushButtonRight(identifier: String, image: NSImage, alternateImage: NSImage?) -> KSTabView {
         _pushButton(identifier, title: nil, image: image, alternateImage: alternateImage, align: .Right)
         return self
@@ -116,9 +116,9 @@ public class KSTabView: NSControl {
         _pushButton(identifier, title: title, image: image, alternateImage: alternateImage, align: .Right)
         return self
     }
-    
+
     private func _pushButton(identifier: String, title: String?, image: NSImage?, alternateImage: NSImage?, align: NSLayoutAttribute) {
-        
+
         var imagePosition: NSCellImagePosition = NSCellImagePosition.NoImage
         if image != nil {
             if align == .Left {
@@ -127,7 +127,7 @@ public class KSTabView: NSControl {
                 imagePosition = rightImagePosition
             }
         }
-        
+
         let coreButton = NSButton(frame: NSZeroRect)
         coreButton.title = title ?? ""
         coreButton.identifier = identifier
@@ -139,7 +139,7 @@ public class KSTabView: NSControl {
         let button = KSButton(aButton: coreButton, tabView: self)
         self.addSubview(button)
         button.translatesAutoresizingMaskIntoConstraints = false
-        
+
         var formatString: String!
         var viewsDictionary: [String: AnyObject]!
         if align == NSLayoutAttribute.Left {
@@ -161,7 +161,7 @@ public class KSTabView: NSControl {
             }
             rightButtonList.append(button)
         }
-        
+
         self.addConstraints(
             NSLayoutConstraint.constraintsWithVisualFormat(
                 formatString,
@@ -177,7 +177,6 @@ public class KSTabView: NSControl {
                 views: ["button" : button])
         )
     }
-    
     func buttonPressed(sender: KSButton) {
         switch selectionType {
         case .One:
@@ -190,7 +189,7 @@ public class KSTabView: NSControl {
             }
         default:()
         }
-        
+
         NSApplication.sharedApplication().sendAction(self.action, to: self.target, from: sender.identifier as NSString?)
     }
 }
@@ -207,7 +206,7 @@ extension KSTabView {
         }
         var underLayer = CAShapeLayer()
         private let selectionLineHeight = CGFloat(3)
-        
+
         private var button: NSButton!
 
         var selected = false {
@@ -215,7 +214,7 @@ extension KSTabView {
                 let activeColor = self.selected ? parentTabView.selectionColor : parentTabView.labelColor
                 button.setAttributedString(parentTabView.fontSize, color: activeColor)
                 button.state = self.selected ? NSOnState : NSOffState
-                
+
                 CATransaction.begin()
                 if self.selected {
                     CATransaction.setAnimationDuration(0.5)
@@ -225,11 +224,11 @@ extension KSTabView {
                 let timing = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)
                 CATransaction.setAnimationTimingFunction(timing)
                 underLayer.strokeStart = 0
-                underLayer.strokeEnd =  self.selected ? 0.5 : 0
+                underLayer.strokeEnd =  self.selected ? 1 : 0
                 CATransaction.commit()
             }
         }
-        
+
         var trackingArea: NSTrackingArea!
         override func updateTrackingAreas() {
             super.updateTrackingAreas()
@@ -239,7 +238,7 @@ extension KSTabView {
             trackingArea = NSTrackingArea(rect: self.bounds, options: [NSTrackingAreaOptions.MouseEnteredAndExited, NSTrackingAreaOptions.ActiveAlways], owner: self, userInfo: nil)
             self.addTrackingArea(trackingArea)
         }
-        
+
         init(aButton: NSButton, tabView: KSTabView) {
             parentTabView = tabView
             super.init(frame: NSZeroRect)
@@ -250,15 +249,15 @@ extension KSTabView {
             self.action = "buttonPressed:"
             self.addSubview(self.button)
             self.button.frame.origin = NSMakePoint(parentTabView.buttonPadding, selectionLineHeight)
-            
+
             let frameWidth = self.button.frame.width + (parentTabView.buttonPadding * 2)
-            
+
             makeUnderLayer(frameWidth)
 
             /// Frame Size
             self.frame.size = NSMakeSize(frameWidth, NSHeight(parentTabView.frame))
         }
-        
+
         func makeUnderLayer(frameWidth: CGFloat) {
 //            let layerWidth = frameWidth - (selectionLineHeight * 2)
             let path = NSBezierPath()
@@ -270,17 +269,17 @@ extension KSTabView {
             underLayer.strokeColor = NSColor.whiteColor().CGColor
             self.layer!.addSublayer(underLayer)
         }
-        
+
         required init?(coder: NSCoder) { fatalError("Init from IB not supported") }
-        
+
         override func mouseEntered(theEvent: NSEvent) { mouseInside = true }
-        
+
         override func mouseExited(theEvent: NSEvent) { mouseInside = false }
-        
+
         override func mouseUp(theEvent: NSEvent) {
             NSApplication.sharedApplication().sendAction(self.action, to: self.target, from: self)
         }
-        
+
         override func drawRect(dirtyRect: NSRect) {
             if mouseInside {
                 parentTabView.hoverColor.setFill()
@@ -299,18 +298,18 @@ extension NSButton {
             self.title = title
             self.imageDimsWhenDisabled = false
         }
-        
+
         required init?(coder aDecoder: NSCoder) { fatalError("init(coder:) has not been implemented") }
 
         override func drawTitle(title: NSAttributedString, withFrame frame: NSRect, inView controlView: NSView) -> NSRect {
             return super.drawTitle(self.attributedTitle, withFrame: frame, inView: controlView)
         }
     }
-    
+
     func setAttributedString(fontSize: CGFloat, color: NSColor) {
         let font = NSFont.labelFontOfSize(fontSize)
         let colorTitle = NSMutableAttributedString(attributedString: self.attributedTitle)
-        
+
         let titleRange = NSMakeRange(0, colorTitle.length)
         colorTitle.addAttribute(NSForegroundColorAttributeName, value: color, range: titleRange)
         colorTitle.addAttribute(NSFontAttributeName, value: font, range: titleRange)
@@ -334,42 +333,26 @@ extension NSButton {
 }
 
 extension NSBezierPath {
+    /// Converts NSBezierPath to CGPath
     var CGPath: CGPathRef {
-        get {
-            return self.convertToCGPath()
-        }
-    }
-    
-    /// Transforms the NSBezierPath into a CGPathRef
-    ///
-    /// - returns: The transformed NSBezierPath
-    private func convertToCGPath() -> CGPathRef {
-        
-        // Create path
         let path = CGPathCreateMutable()
         let points = UnsafeMutablePointer<NSPoint>.alloc(3)
         let numElements = self.elementCount
-        
-        if numElements > 0 {
-            var didClosePath = true
-            for index in 0..<numElements {
-                let pathType = self.elementAtIndex(index, associatedPoints: points)
-                switch pathType {
-                case .MoveToBezierPathElement:
-                    CGPathMoveToPoint(path, nil, points[0].x, points[0].y)
-                case .LineToBezierPathElement:
-                    CGPathAddLineToPoint(path, nil, points[0].x, points[0].y)
-                    didClosePath = false
-                case .CurveToBezierPathElement:
-                    CGPathAddCurveToPoint(path, nil, points[0].x, points[0].y, points[1].x, points[1].y, points[2].x, points[2].y)
-                    didClosePath = false
-                case .ClosePathBezierPathElement:
-                    CGPathCloseSubpath(path)
-                    didClosePath = true
-                }
+
+        for index in 0..<numElements {
+            let pathType = self.elementAtIndex(index, associatedPoints: points)
+            switch pathType {
+            case .MoveToBezierPathElement:
+                CGPathMoveToPoint(path, nil, points[0].x, points[0].y)
+            case .LineToBezierPathElement:
+                CGPathAddLineToPoint(path, nil, points[0].x, points[0].y)
+            case .CurveToBezierPathElement:
+                CGPathAddCurveToPoint(path, nil, points[0].x, points[0].y, points[1].x, points[1].y, points[2].x, points[2].y)
+            case .ClosePathBezierPathElement:
+                CGPathCloseSubpath(path)
             }
-            if !didClosePath { CGPathCloseSubpath(path) }
         }
+
         points.dealloc(3)
         return path
     }
